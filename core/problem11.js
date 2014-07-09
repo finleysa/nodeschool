af8f65eec3e708ce22dfd5a84ff0fa12b8b7e693
Steven Finley <steve.a.finley@gmail.com>
2014-07-09 10:33:21 -0500

    nodeschool finished

--- core/problem11.js
+++ core/problem11.js
@@ -1,11 +1,9 @@
 var http = require('http'),
     port = process.argv[2],
-    fs = require('fs');
-
-console.log(process.argv[3]);
+    map = require('through2-map');
 
 var server = http.createServer(function (req, res){
-  fs.createReadStream(process.argv[3]).pipe(res)
+  res.writeHead(200, {'Content-Type': 'application/json' });
 });
 
 server.listen(port);
